cec36a8079174d1e42ca1930c182b314035a5990
Andrew Capaldo <andycapaldo2@gmail.com>
2025-04-23 15:52:46 -0600

    Refactored loading and null logic into JSX, updated prop names to reflect API changes

--- src/components/MedicalInfo/MedicalInfo.jsx
+++ src/components/MedicalInfo/MedicalInfo.jsx
@@ -3,30 +3,32 @@ import Loader from '../Loader/Loader';
 
 function Card(props) {
     const { drug, isLoading } = props;
-
-    if (isLoading) return <Loader />;
-    if (!drug) return <p>No drug data found.</p>;
+    const {name, brand_name, drug_class, ndc, indications, warnings } = drug || {};
 
     return (
+        <>
+        {isLoading && <Loader />}
+        {!drug && <p>No drug data found.</p>}
         <div className='card'>
             <div className='header'>   
                 <i className="fa-solid fa-tablets fa-2xl icon"></i>
                 <div>
-                    <h3>{drug.Name}</h3>
-                    <p className='subheader'>{drug.Brand_Name}</p>
+                    <h3>{brand_name}</h3>
+                    <p className='subheader'>{name}</p>
                 </div>
             </div>
             <div className='card-body'>
                 <p><span className='label'>Drug Class</span></p>
-                <p className='description'>{drug.Drug_Class}</p>
+                <p className='description'>{drug_class}</p>
                 <p><span className='label'>NDC</span></p>
-                <p className='description'>{drug.NDC}</p>
+                <p className='description'>{ndc}</p>
                 <p><span className='label'>Indications</span></p>
-                <p className='description'>{drug.Indications}</p>
+                <p className='description'>{indications}</p>
                 <p><span className='label'>Warnings</span></p>
-                <p className='description'>{drug.Warnings}</p>
+                <p className='description'>{warnings}</p>
             </div>
         </div>
+        </>
     )
 }
 export default Card;
